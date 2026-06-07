d12f94a8baaf3470d8fb9c59e3905632fff91e53
Gu Qi <lab4ogtc@gmail.com>
2026-06-07 10:55:33 +0800

    feat: 添加项目示例 Metal 版本

---
+++ examples/projects/SpheroControl/main_metal.cpp
@@ -0,0 +1,153 @@
+
+#include "threepp/helpers/CameraHelper.hpp"
+
+#include "threepp/threepp.hpp"
+
+#include "Sphero.hpp"
+#include "SpheroKeyController.hpp"
+
+#include "threepp/renderers/RenderTarget.hpp"
+#include "threepp/utils/ImageUtils.hpp"
+
+#include <iostream>
+
+using namespace threepp;
+
+namespace {
+
+    auto createSprite(const std::shared_ptr<Texture> &texture) {
+        auto spriteMaterial = MeshBasicMaterial::create({{"map", texture}});
+        auto sprite = Mesh::create(PlaneGeometry::create(), spriteMaterial);
+        sprite->scale.set(2, 2, 1);
+
+        return sprite;
+    }
+
+    void addWalls(Scene &scene) {
+        auto wallMaterial = MeshStandardMaterial::create({{"color", Color::black}});
+        auto wall1 = Mesh::create(BoxGeometry::create(0.1, 1, 10), wallMaterial);
+        wall1->position.set(-5, 0.5, 0);
+        scene.add(wall1);
+        auto wall2 = wall1->clone();
+        wall2->translateX(10);
+        scene.add(wall2);
+    }
+
+    void setupScene(Scene &scene) {
+
+        auto light1 = HemisphereLight::create();
+        light1->intensity = 0.8f;
+        scene.add(light1);
+
+        auto grid = GridHelper::create(100, 100);
+        scene.add(grid);
+
+        addWalls(scene);
+
+        auto sphereMaterial = MeshBasicMaterial::create({{"color", Color::blue}});
+        auto sphere1 = Mesh::create(SphereGeometry::create(2.f), sphereMaterial);
+        sphere1->name = "target";
+        sphere1->position.set(0, 2, 50);
+        scene.add(sphere1);
+    }
+
+}// namespace
+
+int main() {
+
+    constexpr int textureSize = 256;
+
+    GlfwWindow canvas("Sphero simulator (Metal)", {{"aa", 8}, {"clientAPI", "Metal"}});
+    auto size = canvas.size();
+    auto renderer = Renderer::create(canvas, Backend::Metal);
+    renderer->autoClear = false;
+
+    Scene scene;
+    scene.background = Color::aliceblue;
+
+    PerspectiveCamera camera(60, canvas.aspect(), 0.1f, 1000);
+    camera.position.set(0, 5, -10);
+    camera.layers.enableAll();
+
+    setupScene(scene);
+
+    Sphero sphero;
+    sphero.position.y = 0.25;
+    scene.add(sphero);
+
+    camera.lookAt(sphero.position);
+    sphero.add(camera);
+
+    auto &spheroCamera = sphero.camera();
+    auto cameraHelper = CameraHelper::create(spheroCamera);
+    scene.add(cameraHelper);
+
+    SpheroKeyController keyController(sphero);
+    canvas.addKeyListener(keyController);
+    std::cout << "Press 'r' to change driving mode. " << std::endl;
+
+    OrbitControls controls(camera, canvas);
+    controls.enableKeys = false;
+    controls.enableZoom = false;
+    controls.enablePan = false;
+
+    Scene orthoScene;
+    OrthographicCamera orthoCamera(-1, 1, 1, -1, 1, 10);
+    orthoCamera.position.z = 1;
+
+    RenderTarget::Options opts;
+    opts.format = Format::RGB;
+    opts.anisotropy = 16;
+    auto renderTarget = RenderTarget::create(textureSize, textureSize, opts);
+    orthoScene.add(createSprite(renderTarget->texture));
+
+    canvas.onWindowResize([&](WindowSize newSize) {
+        camera.aspect = newSize.aspect();
+        camera.updateProjectionMatrix();
+
+        renderer->setSize(newSize);
+
+        size = newSize;
+    });
+
+    Clock clock1, clock2;
+    float imageRefreshInterval = 0.1;
+    auto target = scene.getObjectByName("target");
+    canvas.animate([&] {
+        const auto dt = clock1.getDelta();
+
+        keyController.update();
+        sphero.update(dt);
+
+        controls.target = sphero.position;
+        controls.update();
+
+        target->position.x = 25 * std::sin(math::TWO_PI * 0.05f * clock1.elapsedTime);
+
+        cameraHelper->visible = false;
+        renderer->setRenderTarget(renderTarget.get());
+        renderer->clear();
+        renderer->render(scene, spheroCamera);
+
+        renderer->setRenderTarget(nullptr);
+        renderer->clear();
+        cameraHelper->visible = true;
+        renderer->render(scene, camera);
+
+        renderer->clear(false, true, false);
+        const auto previewY = size.height() > textureSize ? size.height() - textureSize : 0;
+        renderer->setViewport(0, previewY, textureSize, textureSize);
+        renderer->render(orthoScene, orthoCamera);
+        renderer->setViewport(0, 0, size.width(), size.height());
+
+        if (clock2.getElapsedTime() > imageRefreshInterval) {
+
+            renderer->copyTextureToImage(*renderTarget->texture);
+            const auto pixels = renderTarget->texture->image().data();
+            // Pixels can be used for stuff like OpenCV
+
+            clock2.start();
+        }
+    });
+
+}
---
+++ examples/projects/Youbot/youbot_kine_metal.cpp
@@ -0,0 +1,213 @@
+
+#include "threepp/threepp.hpp"
+
+#include "Youbot.hpp"
+#include "kine/Kine.hpp"
+#include "kine/ik/CCDSolver.hpp"
+
+#include "KeyController.hpp"
+#include "threepp/extras/imgui/ImguiContext.hpp"
+#include "threepp/objects/TextSprite.hpp"
+#include "threepp/renderers/Renderer.hpp"
+
+#include <future>
+
+using namespace threepp;
+using namespace kine;
+
+namespace {
+    struct YoubotUI: ImguiContext {
+
+        bool jointMode = true;
+        bool posMode = false;
+
+        Vector3 pos;
+        std::vector<KineLimit> limits;
+        std::vector<float> values;
+
+        explicit YoubotUI(const GlfwWindow& canvas, Renderer& renderer, Kine& kine)
+            : ImguiContext(canvas, renderer),
+              limits(kine.limits()),
+              values(kine.meanAngles()) {
+
+            pos.setFromMatrixPosition(kine.calculateEndEffectorTransformation(values));
+        }
+
+        void onRender() override {
+
+            ImGui::SetNextWindowPos({}, 0, {});
+            ImGui::SetNextWindowSize({}, 0);
+            ImGui::Begin("Youbot");
+
+            ImGui::Text("Target angles");
+            ImGui::SliderFloat("j1", &values[0], *limits[0].min(), *limits[0].max());
+            jointMode = jointMode || ImGui::IsItemEdited();
+            ImGui::SliderFloat("j2", &values[1], *limits[1].min(), *limits[1].max());
+            jointMode = jointMode || ImGui::IsItemEdited();
+            ImGui::SliderFloat("j3", &values[2], *limits[2].min(), *limits[2].max());
+            jointMode = jointMode || ImGui::IsItemEdited();
+            ImGui::SliderFloat("j4", &values[3], *limits[3].min(), *limits[3].max());
+            jointMode = jointMode || ImGui::IsItemEdited();
+            ImGui::SliderFloat("j5", &values[4], *limits[4].min(), *limits[4].max());
+            jointMode = jointMode || ImGui::IsItemEdited();
+
+            posMode = !jointMode;
+
+            ImGui::Text("Target pos");
+            ImGui::SliderFloat("px", &pos.x, -10, 10);
+            posMode = posMode || ImGui::IsItemEdited();
+            ImGui::SliderFloat("py", &pos.y, -10, 10);
+            posMode = posMode || ImGui::IsItemEdited();
+            ImGui::SliderFloat("pz", &pos.z, -10, 10);
+            posMode = posMode || ImGui::IsItemEdited();
+
+            jointMode = !posMode;
+
+            ImGui::End();
+        }
+    };
+
+
+    void setupLights(Scene& scene) {
+        auto light1 = DirectionalLight::create(0xffffff, 1.f);
+        light1->position.set(1, 1, 1);
+        scene.add(light1);
+
+        auto light2 = AmbientLight::create(0xffffff, 1.f);
+        scene.add(light2);
+    }
+}// namespace
+
+int main() {
+
+    GlfwWindow canvas{GlfwWindow::Parameters().title("Youbot-kine (Metal)").size({1280, 720}).antialiasing(8).clientAPI(GlfwWindow::ClientAPI::Metal)};
+    auto renderer = Renderer::create(canvas, Backend::Metal);
+    renderer->autoClear = false;
+    renderer->setClearColor(Color::aliceblue);
+
+    auto scene = Scene::create();
+
+    auto camera = PerspectiveCamera::create(60, canvas.aspect(), 0.01, 100);
+    camera->position.set(-15, 8, 15);
+
+    OrbitControls controls(*camera, canvas);
+
+    auto grid = GridHelper::create(20, 10, Color::yellowgreen);
+    scene->add(grid);
+
+    setupLights(*scene);
+
+    auto endEffectorHelper = AxesHelper::create(1);
+    endEffectorHelper->visible = false;
+
+    auto targetHelper = AxesHelper::create(2);
+    targetHelper->visible = false;
+
+    FontLoader fontLoader;
+    const auto font = *fontLoader.load(std::string(DATA_FOLDER) + "/fonts/typeface/helvetiker_regular.typeface.json");
+
+    TextSprite textHandle(font);
+    textHandle.setColor(Color::black);
+    textHandle.setText("Loading model..");
+    textHandle.setVerticalAlignment(TextSprite::VerticalAlignment::Center);
+    textHandle.setHorizontalAlignment(TextSprite::HorizontalAlignment::Center);
+    textHandle.setWorldScale(20*monitor::contentScale().first);
+
+    HUD hud(*renderer);
+    hud.add(textHandle).setNormalizedPosition({0.5, 0.5});
+
+    TaskManager tm;
+
+    std::shared_ptr<Youbot> youbot;
+    std::unique_ptr<KeyController> keyController;
+    auto loadFuture = std::async(std::launch::async, [&] {
+        try {
+            youbot = Youbot::create(std::string(DATA_FOLDER) + "/models/collada/youbot.dae");
+            youbot->add(targetHelper);
+            youbot->add(endEffectorHelper);
+            endEffectorHelper->visible = true;
+            keyController = std::make_unique<KeyController>(*youbot);
+
+            tm.invokeLater([&] {
+                canvas.addKeyListener(*keyController);
+                scene->add(youbot);
+                textHandle.setText("Use WASD keys to steer robot");
+                hud.getStoredOptions(textHandle)->setNormalizedPosition(0, 0).setVerticalAlignment(HUD::VerticalAlignment::ABOVE).setHorizontalAlignment(HUD::HorizontalAlignment::LEFT);
+            });
+        } catch (const std::exception& e) {
+            tm.invokeLater([&, msg = std::string(e.what())] {
+                textHandle.setText("Error: " + msg);
+            });
+        }
+    });
+
+    canvas.onWindowResize([&](WindowSize size) {
+        camera->aspect = size.aspect();
+        camera->updateProjectionMatrix();
+        renderer->setSize(size);
+    });
+
+    auto ikSolver = CCDSolver(1, 0.001f, 0.00001f);
+    auto kine = KineBuilder()
+                        .addLink(Vector3(1.67, 1.3, 0))
+                        .addRevoluteJoint(Vector3::Y() * -1, {-180.f, 180.f})
+                        .addLink(Vector3(0.33, 1.15, 0))
+                        .addRevoluteJoint(Vector3::Z() * -1, {-90.f, 90.f})
+                        .addLink(Vector3(0, 1.6, 0))
+                        .addRevoluteJoint(Vector3::Z() * -1, {-90.f, 90.f})
+                        .addLink(Vector3(0, 1.3, 0))
+                        .addRevoluteJoint(Vector3::Z() * -1, {-90.f, 90.f})
+                        .addLink(Vector3(0, 0.85, 0))
+                        .addRevoluteJoint(Vector3::Y() * -1, {-180.f, 180.f})
+                        .addLink(Vector3(0, 1.225, 0))
+                        .build();
+
+    YoubotUI ui(canvas, *renderer, kine);
+
+    IOCapture capture{};
+    capture.preventMouseEvent = [] {
+        return ImGui::GetIO().WantCaptureMouse;
+    };
+    canvas.setIOCapture(&capture);
+
+    Clock clock;
+    canvas.animate([&]() {
+        const auto dt = clock.getDelta();
+
+        tm.handleTasks();
+
+        renderer->clear();
+        renderer->render(*scene, *camera);
+
+        hud.render();
+
+        if (youbot) {
+
+            ui.render();
+
+            const auto endEffectorTransformation = kine.calculateEndEffectorTransformation(ui.values);
+            endEffectorHelper->position.setFromMatrixPosition(endEffectorTransformation);
+            endEffectorHelper->quaternion.setFromRotationMatrix(endEffectorTransformation);
+
+            if (ui.jointMode) {
+                ui.pos.setFromMatrixPosition(kine.calculateEndEffectorTransformation(youbot->getJointValues()));
+                targetHelper->visible = false;
+            }
+            if (ui.posMode) {
+
+                auto target = Matrix4().setPosition(ui.pos);
+                target.premultiply(Matrix4().copy(*youbot->matrixWorld).invert());
+                targetHelper->position.setFromMatrixPosition(target);
+                targetHelper->quaternion.setFromRotationMatrix(target);
+                ui.values = ikSolver.solveIK(kine, targetHelper->position, youbot->getJointValues());
+                targetHelper->visible = true;
+            }
+
+            youbot->setJointValues(ui.values);
+            keyController->update(dt);
+        }
+    });
+
+    loadFuture.get();
+
+}
